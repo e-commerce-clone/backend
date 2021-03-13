86109f015b4d9db79750e2c72367e1c775c64791
mungiyo <74698443+mungiyo@users.noreply.github.com>
2021-03-13 21:52:43 +0900

    email_settings.py 개인정보 비공개

--- config/email_settings.py
+++ config/email_settings.py
@@ -5,8 +5,8 @@ EMAIL = {
     'EMAIL_USE_TLS': True,
     'EMAIL_PORT': 587,
     'EMAIL_HOST': 'smtp.gmail.com',
-    'EMAIL_HOST_USER': 'wkdtjdxo2@gmail.com',
-    'EMAIL_HOST_PASSWORD': 'wkd3124265!',
-    'SERVER_EMAIL': 'wkdtjdxo2',
-    'REDIRECT_PAGE': 'https://www.naver.com'
+    'EMAIL_HOST_USER': 'G-mail 아이디',
+    'EMAIL_HOST_PASSWORD': 'G-mail 비밀번호',
+    'SERVER_EMAIL': '서버 이메일(아이디?)',
+    'REDIRECT_PAGE': 'https://www.naver.com'    # redirect할 page
 }
